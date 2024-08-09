341b846e648fd9c77e8100a31861e42015676dd1
Malefic <ogupta4242@gmail.com>
2024-08-08 23:45:45 -0700

    Cleaned everything up

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.serialization) apply false
-}
